7b45f42e80281ae75f0abaa2c2598b23d55bbe43
aldi-computer-home <codealdi@gmail.com>
2024-04-22 08:28:39 +0700

    seeder masing2 user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,12 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Enums\UserRole;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +17,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        DB::table('users')->insert([
+            'name' => 'pegawai TU 1',
+            'email' => 'pegawaitu@contoh.com',
+            'password' => Hash::make('12345678'),
+            'role' => UserRole::pegawaitu,
+        ]);
+        DB::table('users')->insert([
+            'name' => 'Kepala Bagian',
+            'email' => 'kabag@contoh.com',
+            'password' => Hash::make('12345678'),
+            'role' => UserRole::kabag,
+        ]);
+        DB::table('users')->insert([
+            'name' => 'peminjam 1',
+            'email' => 'peminjam1@contoh.com',
+            'password' => Hash::make('12345678'),
+            'role' => UserRole::peminjam,
+        ]);
     }
 }
