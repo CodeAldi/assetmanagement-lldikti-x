5983a4821083a269f82c40da4e925a8a9683fc8d
CodeAldi <codealdi@gmail.com>
2024-12-15 21:52:25 +0700

    menghilangkan padding top dan bottom container content

--- resources/views/layout/main-fluid.blade.php
+++ resources/views/layout/main-fluid.blade.php
@@ -82,7 +82,7 @@
           <div class="content-wrapper">
             <!-- Content -->
 
-            <div class="container-xxl flex-grow-1 container-p-y">
+            <div class="container-xxl flex-grow-1">
               <!-- Layout Demo -->
               <div class="layout-demo-wrapper">
                 @yield('content')
